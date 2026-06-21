26913776c511baa19880e80d2b44909dceb3be67
Alice Severi Gonçalves <aliceseverigoncalves@gmail.com>
2026-06-21 17:22:02 -0300

    delete

--- src/ethopipe/__init__.py
+++
@@ -1,6 +0,0 @@
-"""EthoPipe: Extract, Transform, Load Pipeline for Applied Canine Ethology."""
-
-from ethopipe.models import EthologicalObservation
-from ethopipe.ingestion import load_csv, load_json
-
-__all__ = ["EthologicalObservation", "load_csv", "load_json"]
